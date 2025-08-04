b08cd5bfd337df0e10a98b2791289578691b2ea8
7tps <109924342+7tps@users.noreply.github.com>
2025-08-04 13:03:04 -0400

    initial commit

---
+++ Assets/Tilemaps/RPG Village tileset/tiled file/interior.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="interior" tilewidth="16" tileheight="16" tilecount="500" columns="20">
+ <image source="../Tileset/interior.png" width="320" height="400"/>
+ <terraintypes>
+  <terrain name="Room" tile="21"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain="0,0,0,"/>
+ <tile id="4" terrain="0,0,,0"/>
+ <tile id="20" terrain=",0,,0"/>
+ <tile id="21" terrain="0,0,0,0"/>
+ <tile id="22" terrain="0,,0,"/>
+ <tile id="23" terrain="0,,0,0"/>
+ <tile id="24" terrain=",0,0,0"/>
+ <tile id="40" terrain=",0,,"/>
+ <tile id="41" terrain="0,0,,"/>
+ <tile id="42" terrain="0,,,"/>
+</tileset>
