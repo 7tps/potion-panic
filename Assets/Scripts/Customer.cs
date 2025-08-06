160e04a364983eca36a187615a9068038622966a
7tps <109924342+7tps@users.noreply.github.com>
2025-08-06 11:59:39 -0400

    update Customer.cs

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -52,7 +52,6 @@ public class Customer : MonoBehaviour
         bubble = b;
         waitTime = w;
         sr.sprite = customerSprite;
-        requestTime = Time.time;
     }
 
     void RequestOrder()
