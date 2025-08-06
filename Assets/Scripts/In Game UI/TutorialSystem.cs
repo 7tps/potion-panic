d33c63733766f295cf3b5ebe2747ba62c8dd3050
JamesChen22 <chenpinzhang122@gmail.com>
2025-08-06 15:36:48 -0400

    level 1 tutorial

--- Assets/Scripts/In Game UI/TutorialSystem.cs
+++ Assets/Scripts/In Game UI/TutorialSystem.cs
@@ -11,25 +11,85 @@ public class TutorialSystem : MonoBehaviour
     public int numOfTutorials;
     public bool completed = false;
     public int index = 0;
+    public int tutorialCount = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        print("POPOPOPOPOP");
+        //print("POPOPOPOPOP");
         startTime = Time.time;
         UIManager.instance.ShowPopupMenu("Welcome!", "Welcome to Potion Panic! Here's a quick tutorial to get you started!\n");
         tutorialTexts.Add("Look below, customers will come to your potion shop very soon, they will order a potion with the specific color.");
         tutorialTexts.Add("The timer on the upper-left shows the total time you have.\n\nYou can pause the game using the button on the upper-right or Esc key");
         tutorialTexts.Add("The pause screen shows the recipes of potions that customers might order in this level.");
+        tutorialTexts.Add("A customer just showed up! They ordered an orange potion.\n\nRemember you may check the recipes in the pause screen.");
+        tutorialTexts.Add("Go to one of the crates, press Spacebar to take out an ingredient out of the crate\n\nPress Spacebar again to pick up the ingredient.");
+        tutorialTexts.Add("Go in front of the pot to put the ingredient in.");
+        tutorialTexts.Add("Go to the bottle cabinet to get a potion bottle.\n\nThen fill it in front of the pot.\n\nAfter filling, hand it to the customer!");
         numOfTutorials = tutorialTexts.Count;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time - startTime >= 1.0f && !completed)
+        if (!completed)
         {
-            startTime = Time.time;
+            if (index < 3)
+            {
+                if (Time.time - startTime >= 0.001f)
+                {
+                    startTime = Time.time;
+                    showNextTutorial();
+                }
+            }
+            if (index == 3)
+            {
+                if (Time.time - startTime >= 17.0f)
+                {
+                    startTime = Time.time;
+                    showNextTutorial();
+                }
+            }
+            if (index == 4)
+            {
+                if (Time.time - startTime >= 2.0f)
+                {
+                    startTime = Time.time;
+                    showNextTutorial();
+                }
+            }
+            if (index == 5)
+            {
+                if (Time.time - startTime >= 3.0f)
+                {
+                    startTime = Time.time;
+                    showNextTutorial();
+                }
+            }
+            if (index == 6)
+            {
+                if (Time.time - startTime >= 6.0f)
+                {
+                    startTime = Time.time;
+                    showNextTutorial();
+                }
+            }
+            if (index > 6)
+            {
+                if (Time.time - startTime >= 2.0f)
+                {
+                    startTime = Time.time;
+                    showNextTutorial();
+                }
+            }
+        }
+    }
+
+    void showNextTutorial()
+    {
+        if (!completed)
+        {
             UIManager.instance.ShowPopupMenu("Tutorial", tutorialTexts[index]);
             index += 1;
             if (index == numOfTutorials)
