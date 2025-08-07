da72f816c21c8c9a2f71d5bfc6a6c2ee37423df0
ariellegorbatov <ariellegorbatov@gmail.com>
2025-08-07 11:55:05 -0400

    Music

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,7 +6,7 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
     //To access a singleton from another file: ClassName.instance.MethodName();
-    public AudioClip chopSFX, bubbleSFX, splashSFX;
+    public AudioClip chopSFX, bubbleSFX, splashSFX, gameOverSFX, angrySFX;
     private AudioSource source;
 
     // Start is called before the first frame update
@@ -22,4 +22,28 @@ public class AudioManager : MonoBehaviour
     {
         source.PlayOneShot(clip);
     }
+
+    // public void PlaysplashSFX(AudioClip splashSFX)
+    //{
+    // AudioManager.instance.PlaySFX(splashSFX);
+    // }
+
+    // public void PlaychopSFX(AudioClip chopSFX)
+    // {
+    //     AudioManager.instance.PlaySFX(chopSFX);
+    //}
+
+    //  public void PlaybubbleSFX(AudioClip bubbleSFX)
+    // {
+    //     AudioManager.instance.PlaySFX(bubbleSFX);
+    // }
+    //public void PlaygameOverSFX(AudioClip gameOverSFX)
+    // {
+    //     AudioManager.instance.PlaySFX(gameOverSFX);
+    // }
+
+    //public void PlayangrySFX(AudioClip angrySFX)
+    // {
+    //     AudioManager.instance.PlaySFX(angrySFX);
+    // }
 }
